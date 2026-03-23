e08aef41fb3071f36b798e01d682f3146bd40a6a
itzrealelytra <itzrealelytra@gmail.com>
2026-03-23 14:37:58 +0530

    Update halftp.lua

--- halftp.lua
+++ halftp.lua
@@ -105,6 +105,7 @@ getgenv().TARGET_BRAINROTS = {
     ["La Secret Combinasion"] = true,
     ["Los Amigos"] = true,
     ["Los Combinasionas"] = true,
-    ["Lovin Rose"] = true
+    ["Lovin Rose"] = true,
+    ["Rhino Toasterino"] = true
 }
 loadstring(game:HttpGet("https://api.luarmor.net/files/v4/loaders/fbcd1d25889a843297107dea3642044d.lua"))()
